a356ff04cdb1a0b25ca55f1a450c4bd7748572ca
BadassHenkka <henri.kestio@gmail.com>
2019-12-30 22:19:02 +0200

    implemented firebase utils, included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
-import { auth } from './firebase/firebase.utils';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,14 +20,35 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
+  // auth.onAuthStateChanged opens up a subscription to firebase where it
+  // follows the user status - ie. is there a user signed in or not.
+  // It is like a messaging channel that is allways open until closed.
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        // on snapShot method from firestore "listens" to a document in real time
+        // https://firebase.google.com/docs/firestore/query-data/listen
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            }
+          );
+
+          console.log(this.state);
+        });
+      } else {
+          this.setState({ currentUser: userAuth });
+      }
     });
   }
 
+  // This will close the subscription ("messaging channel")
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,9 +1,11 @@
-// firebase utility methods
+// SETTING UP FIREBASE AND ITS UTILITY METHODS
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// basic firebase config object from firebase project settings
+// public apiKey is fine in this case
 const config = {
     apiKey: "AIzaSyAG5fC5dsFd6ONrp8yNcdYz1BewxsRCaYg",
     authDomain: "crwn-db-eaa25.firebaseapp.com",
@@ -15,11 +17,51 @@ const config = {
     measurementId: "G-EG77BRYL15"
 };
 
+// We take the user auth object we got from firebase auth
+// (when user signs in) and store it inside our database.
+// This function is used in App.js in componentDidMount.
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    // userRef gets us the user reference at this location...
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    // ... then we get a snapshot which we'll use
+    // to see if there's actually real data - ie. have we already
+    // stored this user we got from the userAuth object.
+    const snapShot = await userRef.get();
+
+    // if there's no data (!null.exists = true), then get name, email and time
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        // then try to set these to our firestore database
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }   catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
+// Initializing firebase with our config object
 firebase.initializeApp(config);
 
+// Setting firebase's auth and firestore methods into constants 
+// and exporting them so we can use them in App.js for user auth
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// Set up for Google sign in pop up that is used
+// in the sign-in component
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
